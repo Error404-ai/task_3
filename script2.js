d9a068422c16a2133d06708f03f714cb6e7c7545
Tanisha Bhatt <tanisha2310187@akgec.ac.in>
2024-11-15 00:06:10 +0530

    task_3

--- script2.js
+++ script2.js
@@ -1,6 +1,33 @@
 const canvas = document.getElementById('canvas1');
 // instance of creating canvas api for building all types of 2d objects
 const ctx = canvas.getContext('2d'); 
+const plantImage = new Image;
+plantImage.src = 'WhatsApp Image 2024-11-14 at 21.50.10_65bf9754.jpg'
+
+const zombieImage = new Image;
+zombieImage.src = 'WhatsApp_Image_2024-11-14_at_21.50.10_f780f442-removebg-preview.png'
+
+const zombieHitsound = new Audio('HitSound.wav');
+const bonusSound = new Audio('Bonus.mp3');
+const startButton = document.getElementById('start');
+let gameStart = false;
+
+startButton.addEventListener('click',function(){
+    startButton.style.display= 'none';
+    gameStart= true;
+    gameOver = false;
+
+    // Promise.all([ 
+    //     new Promise((resolve) => { plantImage.onload = resolve }),
+    //     new Promise((resolve) => { zombieImage.onload = resolve })
+    // ]).then(() => {
+    //     animate();  // Start the game animation once all images are loaded
+    // });
+    animate();
+})
+
+
+
 canvas.width = 900;
 canvas.height = 600;
 
@@ -9,7 +36,7 @@ const cellSize = 100;
 const cellGap= 3;
 
 let resources = 200;
-let interval = 400;
+let interval = 600;
 let frame = 0;
 let gameOver = false;
 let score = 0;
@@ -23,7 +50,78 @@ const hitter = [];
 const zombiePos = [];
 const power = [];
 
-class Hitters{ //template for creation of object to hit zombies
+
+
+
+//mouse
+const mouse ={
+    x : 10,
+    y: 10,
+    width : 0.1,
+    height : 0.1
+
+}
+
+let canvasPosition = canvas.getBoundingClientRect(); // this function returns a built in rectangle project which contains info about the size of the element aand its position 
+canvas.addEventListener('mousemove', function(e){
+    mouse.x = e.x - canvasPosition.left;
+    mouse.y = e.y - canvasPosition.top;
+});
+canvas.addEventListener('mouseleave', function(){
+    mouse.x = null;
+    mouse.y = null;
+});
+
+// game board 
+const controlsBar = {
+    width: canvas.width,
+    height: cellSize,
+}
+class Cell {
+constructor(x,y){
+    this.x = x;
+    this.y = y;
+    this.width = cellSize;
+    this.height = cellSize;
+}
+draw(){
+    if (mouse.x && mouse.y && collision(this,mouse)){
+        ctx.strokeStyle = 'black';
+        ctx.strokeRect(this.x,this.y,this.width,this.height);
+    }
+}
+}
+function createGrid(){
+    for(let y = cellSize;y<canvas.height;y+=cellSize){
+        for(let x = 0; x<canvas.width; x +=cellSize){
+            gameGrid.push(new Cell(x,y));
+        }
+    }
+}
+createGrid();
+function handleGameGrid(){
+    for(let i=0;i<gameGrid.length;i++){
+        gameGrid[i].draw();
+    }
+}
+
+
+
+//utilities
+
+function collision(first, second){
+    if (    !(  first.x > second.x + second.width ||
+                first.x + first.width < second.x ||
+                first.y > second.y + second.height ||
+                first.y + first.height < second.y)
+    ) {
+        return true;
+    };
+};
+
+
+
+class Hitters { //template for creation of object to hit zombies
     constructor(x,y){
         this.x = x;
         this.y = y;
@@ -36,9 +134,9 @@ class Hitters{ //template for creation of object to hit zombies
         this.x += this.speed;
     }
     draw(){
-        ctx.fillStyle = 'dark green'; //the fill color is set to dark green
         ctx.beginPath(); //to start a new path (not disturbing any other drawing)
         ctx.arc(this.x, this.y, this.width, 0, Math.PI * 2);
+        ctx.fillStyle = 'black'; //the fill color is set to dark green
         ctx.fill(); //color is filled in the element
     }
 }
@@ -56,8 +154,10 @@ function handleHitters(){
                 zombie[j].health -= hitter[i].power;
                 hitter.splice(i,1); //getting rid of the hitter that has hit the zombie
                 i--;
+                zombieHitsound.play();
             }
         }
+        
         if(hitter[i] && hitter[i].x > canvas.width - cellSize)
         {
             hitter.splice(i, 1);
@@ -78,19 +178,16 @@ class plants{ //template for creation of plants
         this.timer = 0; //timer to set interval for shooting hitter
     }
     draw(){
-        ctx.fillStyle = 'green'; //color of plant
-        ctx.fillRect(this.x, this.y, this.width, this.height);
-        ctx.font = '30px Arial';
-        ctx.fillStyle = 'gold'; //color of text inside
-        ctx.fillText(Math.floor(this.health), this.x + 15 , this.y + 30);
-        
+
+        ctx.drawImage(plantImage,this.x,this.y,this.width,this.height);
+       
     }
     update(){
         if(this.shooting)
         {
             this.timer++;
             if(this.timer % 100 === 0)
-            hitter.push(new Hitters(this.x + 75, this.y + 50)); //new hitter shooting after every 100 values of the timer
+            hitter.push(new Hitters(this.x + 75, this.y + 35)); //new hitter shooting after every 100 values of the timer
         }
         else
         this.timer = 0;
@@ -142,7 +239,7 @@ function handlePlants(){
         }     
     }
 }
-
+//zombies
 class zombies{ //template for creation of zombies
     constructor(verticalPos){
         this.x = canvas.width;
@@ -158,12 +255,7 @@ class zombies{ //template for creation of zombies
         this.x -= this.move; //modification in the position of zombie
     }
     draw(){ //to render the zombie on the canvas
-        ctx.fillStyle = 'brown';
-        ctx.fillRect(this.x, this.y, this.width, this.height);
-        ctx.font = '30px Arial';
-        ctx.fillStyle = 'gold';
-        ctx.fillText(Math.floor(this.health), this.x + 15, this.y + 30);
-        
+        ctx.drawImage(zombieImage,this.x,this.y,this.width,this.height);
     }
 }
 
@@ -177,7 +269,7 @@ function handleZombies(){
         {
             let bonus = zombie[i].maxHealth / 10; //power gained by plant after killing a zombie
             resources += bonus;
-            score += bonus;
+            score +=bonus;
             zombie.splice(i,1);
             zombiePos.splice((zombiePos.indexOf(zombie[i].y)), 1);
             i--;
@@ -185,7 +277,7 @@ function handleZombies(){
     }
     if(frame % interval === 0) //new zombie will be added after every interval
     {
-        let verticalPos = Math.floor(Math.random() * 5 + 1) * cellSize; 
+        let verticalPos = Math.floor(Math.random() * 5 + 1) * cellSize + cellGap; 
         //for random selection of row for zombie movement
         zombie.push(new zombies(verticalPos));
         zombiePos.push(verticalPos);
@@ -197,8 +289,8 @@ function handleZombies(){
 const values = [20, 30, 40];
 class Power{ 
     constructor(){ //template for randomly occurring power packets
-        this.x = (Math.random() * canvas.width) - cellSize;
-        this.y = (Math.floor(Math.random() * 5) + 1) * cellSize;
+        this.x = Math.random() * (canvas.width - cellSize);
+        this.y = (Math.floor(Math.random() * 5) + 1) * cellSize + 25;
         this.width = cellSize * 0.6;
         this.height = cellSize * 0.6;
         this.value = values[Math.floor(Math.random() * values.length)];
@@ -206,36 +298,60 @@ class Power{
     draw(){
         ctx.fillStyle = 'yellow';
         ctx.fillRect(this.x, this.y, this.width, this.height);
-        ctx.font = '30px Arial';
         ctx.fillStyle = 'black';
+        ctx.font = '30px Arial';
         ctx.fillText(this.value, this.x + 15, this.y + 50);
     }
 }
 
 function handlePower(){
     if(frame % 500 === 0)
-        power.push(new Power);
+        power.push(new Power());
     for(let i = 0 ; i < power.length ; i++)
     {
         power[i].draw();
         if(power[i] && mouse.x && mouse.y && collision(power[i], mouse))
         {
-            resources += power[i].values;
+            resources += power[i].value;
             power.splice(i, 1); 
             i--;
+            bonusSound.play();
         }
     }
 }
 
 function gameStatus(){
+    
     ctx.font = '30px Arial';
     ctx.fillStyle = 'black';
-    ctx.fillText('Score: '+score, 20, 30);
-    ctx.fillText('Power: '+resources, 20 , 80);
+    ctx.fillText('Score: ' +score, 20, 30);
+    ctx.fillText('Power:' +resources, 20 , 80);
     if(gameOver)
     {
+       
         ctx.font = '100px Arial';
         ctx.fillStyle = 'black';
         ctx.fillText('GAME OVER', 170, 350);
     }
 }
+function animate(){
+    if(!gameStart)
+        return;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = 'lavender';
+  ctx.fillRect(0,0,controlsBar.width,controlsBar.height);
+  
+  handleGameGrid();
+  handlePlants();
+  handlePower();
+  handleHitters();
+  handleZombies();
+  gameStatus();
+  frame++;
+  if (!gameOver) requestAnimationFrame(animate);
+}
+
+
+window.addEventListener('resize', function(){
+    canvasPosition = canvas.getBoundingClientRect();
+})
